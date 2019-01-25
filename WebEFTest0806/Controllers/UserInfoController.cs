d97b3b8ac7ac27333ddfed69034cdf6bffc7894f
admin <admin@WIN10>
2019-01-25 16:29:02 +0800

    用户注册界面

--- WebEFTest0806/Controllers/UserInfoController.cs
+++ WebEFTest0806/Controllers/UserInfoController.cs
@@ -1,4 +1,5 @@
 ﻿using EFDal;
+using EFBll;
 using Model;
 using System;
 using System.Collections.Generic;
@@ -11,26 +12,65 @@ namespace WebEFTest0806.Controllers
 {
     public class UserInfoController : Controller
     {
-        UserInfoDal userInfoDal = new UserInfoDal();
+        UserInfoService userInfoBll = new UserInfoService();
         public ActionResult Index()
         {
-            List<UserInfo> userInfoList = new List<UserInfo>();
 
-            userInfoList.Add(new UserInfo { Age = 12, UserName = "hello world",  UserMail = "1234567@qq.com", UserRealName = "good " });
-            userInfoList.Add(new UserInfo { Age = 13, UserName = "hello world1", UserMail = "1234567@qq.com", UserRealName = "good1" });
-            userInfoList.Add(new UserInfo { Age = 14, UserName = "hello world2", UserMail = "1234567@qq.com", UserRealName = "good2" });
-
-
-
-            ViewData["userInfoList"] = userInfoList;
-
+            ViewData["UserList"] = userInfoBll.GetEntities(u => u.Id > 2).ToList();
+            var users = ViewData["UserList"];
             return View();
         }
 
-        public ActionResult Login()
+        public ActionResult LoadUserInfos()
         {
+
+            var userList = userInfoBll.GetEntities(u => u.Id > 2).Select(u => new { u.UserLoginName, u.UserName, u.Id }).ToList();
+
+            return Json(userList, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AddUser()
+        {
+
             return View();
         }
 
+        public string AddUserInfo(Model.UserInfo userInfo)
+        {
+
+            if (userInfo.UserLoginName == "" || userInfo.UserLoginName == null)
+            {
+
+                return ViewBag.Errmsg = "no:请输入用户登录名！";
+            }
+            if (userInfo.Age > 120 || userInfo.Age < 0 || userInfo.Age == null)
+            {
+                return ViewBag.Errmsg = "no:请输入正确的年龄！";
+            }
+            if (userInfo.UserMail == "" || userInfo.UserMail == null)
+            {
+                return ViewBag.Errmsg = "no:请输入邮箱！";
+            }
+
+            if (userInfo.UserName == "" || userInfo.UserName == null)
+            {
+                return ViewBag.Errmsg = "no:请输入用户真实名！";
+            }
+
+            if (userInfo.UserPsd == "" || userInfo.UserPsd == null)
+            {
+                return ViewBag.Errmsg = "no:请输入密码！";
+            }
+
+            if (userInfoBll.Add(userInfo))
+            {
+                return ViewBag.Errmsg = "ok:添加成功";
+            }
+            else
+            {
+                return ViewBag.Errmsg = "ok:添加失败";
+            }
+
+        }
     }
 }
